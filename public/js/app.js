b44534135e4bbd60de4bc73ee61403a7d5900639
Rakshit Vyas <rkvyas98@gmail.com>
2022-03-30 01:03:19 +0530

    edit about page

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const location= search.value
 
-    messageOne.textContent = 'Loading data...'
+    messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
     fetch('/weather?address='+location).then((response)=>{
--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res)=>{
 app.get('/about', (req, res)=>{
     res.render('about',{
         title:'About Me',
-        img: '/img/robot.png',
+        img: '/img/rv.jpg',
         name: 'Rakshit Vyas'
     })
 })
